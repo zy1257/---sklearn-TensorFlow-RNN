3ef9818801adbba9cb6feae027dbf4ea349a0ec1
Zhangyu <47965142+zy1257@users.noreply.github.com>
2019-04-17 00:30:44 +0800

    Update model_RNN.py

--- model_RNN.py
+++ model_RNN.py
@@ -4,19 +4,21 @@ import numpy as np
 import pickle
 from sklearn import metrics
 
+
 class RNN_Model():
-    def __init__(self, train_dir,test_dir,val_dir,vocab):
+    def __init__(self, train_dir,test_dir,val_dir,vocab,embedding):
         self.train_dir = train_dir
         self.test_dir = test_dir
         self.val_dir = val_dir
         self.save_path = '.\\model/RNN.model'
-        self.dropout = 1.0
-        self.batch_size = 128
+        self.dropout = 0.8
+        self.batch_size = 64
         self.max_length = 600
-        self.embedding_dim = 100
         self.lr = 0.001
-        self.vocab = vocab
         self.cell_num = 128
+        self.epoch = 1
+        self.embedding = embedding
+        self.vocab = vocab
         self.categories = {'体育': 0, '财经': 1, '房产': 2, '家居': 3, '教育': 4,
                           '科技': 5, '时尚': 6, '时政': 7, '游戏': 8, '娱乐': 9}
         self.cat_num = len(self.categories)
@@ -53,20 +55,20 @@ class RNN_Model():
         self.input_y = tf.placeholder(tf.float32, [self.batch_size, self.cat_num], name='input_y')
         self.keep_prob = tf.placeholder(tf.float32, name='keep_prob')
 
-        embedding = tf.get_variable('embedding', [len(self.vocab), self.embedding_dim])
+        embedding = tf.Variable(self.embedding)
         embedding_inputs = tf.nn.embedding_lookup(embedding, self.input_x)
 
         cells = []
         for _ in range(2):
-            cell = tf.contrib.rnn.GRUCell(128)
+            cell = tf.contrib.rnn.GRUCell(self.cell_num)
             cell = tf.contrib.rnn.DropoutWrapper(cell, output_keep_prob=self.dropout)
             cells.append(cell)
         rnn_cell = tf.contrib.rnn.MultiRNNCell(cells, state_is_tuple=True)
-        _outputs, _ = tf.nn.dynamic_rnn(cell=rnn_cell, inputs=embedding_inputs, dtype=tf.float32)
+        _outputs, _ = tf.nn.dynamic_rnn(cell=rnn_cell, inputs=embedding_inputs, dtype=tf.float64)
         last = _outputs[:,-1,:]
 
-        fc = tf.layers.dense(last, 128, name='fc1')
-        fc = tf.contrib.layers.dropout(fc, 1.0)
+        fc = tf.layers.dense(last, self.cell_num, name='fc1')
+        fc = tf.contrib.layers.dropout(fc, self.dropout)
         logits = tf.layers.dense(fc, 10, name='fc2')
         self.y_pred = tf.argmax(tf.nn.softmax(logits), 1)
 
@@ -87,22 +89,26 @@ class RNN_Model():
         writer = tf.summary.FileWriter(self.summary_path, sess.graph)
         best_acc = 0.0
         val_acc = []
-        xy_train = self.process_file(self.train_dir)
-        for step,(x_train, y_train) in enumerate(xy_train):
-            w_merge, _ ,train_acc= sess.run([merge,self.optim,self.acc],
-                                     feed_dict={self.input_x:x_train,self.input_y:y_train})
-            writer.add_summary(w_merge, step)
-            if train_acc > 0.95:
-                xy_val = self.process_file(self.val_dir)
-                for x_val, y_val in xy_val:
-                    val_acc.append(sess.run(self.acc,
-                                   feed_dict={self.input_x:x_val,self.input_y:y_val}))
-                Acc = sum(val_acc)/len(val_acc)
-                print('val_acc',Acc,'\t','train_acc',train_acc)
-                if Acc > best_acc:
-                    best_acc = Acc
-                    saver.save(sess, save_path = self.save_path)
-        print('best_acc:',best_acc)
+        
+        for epoch in range(self.epoch):
+            print('epoch{}'.format(epoch))
+            xy_train = self.process_file(self.train_dir)
+            for step,(x_train, y_train) in enumerate(xy_train):
+                w_merge, _ ,train_acc= sess.run([merge,self.optim,self.acc],
+                                         feed_dict={self.input_x:x_train,self.input_y:y_train})
+                writer.add_summary(w_merge, step)
+                if train_acc > 0.92:
+                    xy_val = self.process_file(self.val_dir)
+                    for x_val, y_val in xy_val:
+                        val_acc.append(sess.run(self.acc,
+                                       feed_dict={self.input_x:x_val,self.input_y:y_val}))
+                    Acc = sum(val_acc)/len(val_acc)
+                    print('val_acc',Acc,'\t','train_acc',train_acc)
+                    if Acc > best_acc:
+                        best_acc = Acc
+                        print('best_acc',best_acc)
+                        saver.save(sess, save_path = self.save_path)
+
 
     def test(self):
         saver = tf.train.Saver()
@@ -125,10 +131,12 @@ class RNN_Model():
 train_dir = '.\\cnews\\cnews.train_shuffle.txt'
 test_dir = '.\\cnews\\cnews.test.txt'
 val_dir = '.\\cnews\\cnews.val.txt'
-with open('.\\train_vocab.pkl','rb') as f:
+with open('.\\vocab.pkl','rb') as f:
     vocab = pickle.load(f)
-model = RNN_Model(train_dir,test_dir,val_dir,vocab)
+with open('embedding.pkl','rb') as f:
+    embedding = pickle.load(f)
+model = RNN_Model(train_dir,test_dir,val_dir,vocab, embedding)
 model.build_graph()
-##model.train()
-model.test()
+model.train()
+##model.test()
 
