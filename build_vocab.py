932d820e23b195f175dacad6f8f23a71708c8608
Zhangyu <47965142+zy1257@users.noreply.github.com>
2019-04-17 00:38:12 +0800

    Update build_vocab.py

--- build_vocab.py
+++ build_vocab.py
@@ -1,23 +1,64 @@
-from collections import Counter
 import pickle
-train_dir = '.\\cnews\\cnews.train.txt'
+import numpy as np
+from collections import Counter
 
-def read_vocab(vocab_dir):
-    with open(vocab_dir, encoding ='utf-8') as f:
+def build_vocab_oneword():
+    vocab_f = {}
+    f = open('sgns.renmin.word','r',encoding='utf-8')
+    for i in f:
+        if i[1] == ' ':
+            char = i[0]
+            vec = i[2:].strip().split(' ')
+            for i in range(300):
+                vec[i] = float(vec[i])
+            vocab_f[char] = vec
+    f.close()
+    with open('vocab_oneword.pkl','wb')as f:
+        pickle.dump(vocab_f,f)
+
+def build_vocab_stop():
+    with open('vocab_oneword.pkl','rb') as f:
+        vocab_f = pickle.load(f)
+    vocab_stop=[]
+    with open('cnews\\cnews.train.txt','r',encoding='utf-8') as f:
         for i in f:
-            _, s = i.split(sep='\t')
-            yield s
+            _, content = i.split(sep='\t')
+            for j in content:
+                if j not in vocab_f and j not in vocab_stop:
+                    vocab_stop.append(j)
+    vocab_stop = dict(Counter(vocab_stop))
+    with open('vocab_stop.pkl','wb') as f:
+        pickle.dump(vocab_stop,f)
 
-def build_vocab(train_dir):
-    s = read_vocab(train_dir)
-    vocab = Counter(next(s))
-    for i in s:
-        vocab.update(Counter(i))
-    keys = vocab.most_common(5000)
+def build_vocab():
+    with open('vocab_stop.pkl','rb')as f:
+        vocab_stop = pickle.load(f)
     vocab = {}
-    for i,(key,_) in enumerate(keys):
-        vocab[key] = i
-    with open('train_vocab.pkl','wb') as f:
+    with open('cnews\\cnews.train.txt','r',encoding='utf-8') as f:
+        n = 0
+        for i in f:
+            _, content = i.split(sep='\t')
+            for j in content:
+                if j not in vocab_stop and j not in vocab:
+                    vocab[j] = n
+                    n += 1
+    with open('vocab.pkl','wb')as f:
         pickle.dump(vocab,f)
 
-build_vocab(train_dir)
+def build_embedding_np():
+    with open('vocab.pkl','rb')as f:
+        vocab = pickle.load(f)
+    with open('vocab_oneword.pkl','rb')as f:
+        vocab_oneword = pickle.load(f)
+    embedding = []
+    keys = vocab.keys()
+    for i in keys:
+        embedding.append(vocab_oneword[i])
+    embedding = np.array(embedding)
+    with open('embedding.pkl','wb') as f:
+        pickle.dump(embedding,f)
+
+build_vocab_oneword()
+build_vocab_stop()
+build_vocab()
+build_embedding_np()
